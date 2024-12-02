4eb3893e3ea80d10141a58ba0313b6d571444a28
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-02 20:50:17 -0300

    cria novos componentes (animais)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 
 function App() {
   return <div className="app"> 
-  <Animal icone="🦭" nome="Foca" peso={-1} extincao={true}/>
+  <Animal icone="🦭" nome="Foca" peso={97.3} extincao={true}/>
+  <Animal icone="🐋" nome="Baleia" peso={147.9} extincao={true}/>
+  <Animal icone="🦈" nome="Tubarão" peso={3445.69} extincao={false}/>
   </div>;
 }
 
