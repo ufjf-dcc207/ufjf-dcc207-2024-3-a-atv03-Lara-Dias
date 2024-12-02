9d49c42eb4c2204475ecca5eb756699654e8ba0d
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-02 20:45:10 -0300

    desenho duas alternativas

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -8,10 +8,16 @@ interface AnimalProps{
 }
 
 export default function Animal({icone, nome, peso, extincao}:AnimalProps){
-    return <div className="animal">
-        <div className="icone">{icone}</div>
-        <div className="nome">{nome}</div>
-        <div className="peso">{peso+" Kg"}</div>
-        <div className="extincao">{extincao}</div>
-    </div>
+    if(extincao){
+        return <div className="animal">
+            <div className="icone">{icone}</div>
+            <div className="nome">{nome}</div>
+            {peso>0.0?(
+                <div className="peso">{peso.toLocaleString()+" Kg"}</div>
+            ):(
+                <div className="peso">Desconhecido</div>
+            )}
+            {extincao && <div className="extincao">Em extinção</div>}
+        </div>
+    }
 }
