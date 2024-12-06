fad2bffd5f0dd553da2a5842c18213c81bcfff6a
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-05 21:50:19 -0300

    cria componente exibição e estiliza

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Animal from './Animal'
+import Exibicao from './Exibicao';
 import './App.css'
 
 function App() {
-  return <div className="app"> 
-  <Animal icone="🦭" nome="Foca" peso={97.3} extincao={true}/>
-  <Animal icone="🐋" nome="Baleia" peso={147.9} extincao={true}/>
-  <Animal icone="🦈" nome="Tubarão" peso={3445.69} extincao={false}/>
-  </div>;
+  return (
+    <div className="app">
+      <Exibicao abertura= {new Date("2024-11-05T08:00-03:00")} fechamento = {new Date("2024-11-05T16:00-03:00")} cercado="A1"/>
+        <Animal icone="🦭" nome="Foca" peso={97.3} extincao={true}/>
+        <Animal icone="🐋" nome="Baleia" peso={147.9} extincao={true}/>
+      <Exibicao abertura= {new Date("2024-11-05T08:00-03:00")} fechamento = {new Date("2024-11-05T16:00-03:00")} cercado="B1"/>
+      <Animal icone="🦈" nome="Tubarão" peso={3445.69} extincao={true}/>
+  
+    </div>
+  );
 }
 
 export default App
